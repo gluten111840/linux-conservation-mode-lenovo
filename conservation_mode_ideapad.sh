314648e244b031354c93a3acb171164950af9f0d
Bayu Eka Prawira <58933506+gluten111840@users.noreply.github.com>
2023-08-16 18:37:28 +0700

    fix: fix string comparison

--- conservation_mode_ideapad.sh
+++ conservation_mode_ideapad.sh
@@ -5,34 +5,27 @@ con_mode=$(cat "$conservation_mode_location")
 on="1"
 off="0"
 
-echo "================ CONSERVATION MODE ================"
+echo "================ CONSERVATION MODE ================
+"
 
 if [[ $con_mode -eq $on ]]; then
     echo "Currently, the conservation mode is ON
 Do you want to turn OFF the conservation mode? [Y/N]:"
     read -r VAR1
-    if [[ $VAR1 -eq "Y" || $VAR1 -eq "y" ]]; then
+    if [[ $VAR1 == "Y" || $VAR1 == "y" ]]; then
         echo "$off" > "$conservation_mode_location"
-        echo "Conservation mode turned OFF
-
-================ CONSERVATION MODE ================"
+        echo "Conservation mode turned OFF"
     else
-        echo "Bye
-
-================ CONSERVATION MODE ================"
+        echo "Bye"
     fi
 else
     echo "Currently, the conservation mode is OFF
 Do you want to turn on the conservation mode? [Y/N]:"
     read -r VAR2
-    if [[ $VAR2 -eq "Y" || $VAR2 -eq "y" ]]; then
+    if [[ $VAR2 == "Y" || $VAR2 == "y" ]]; then
         echo "$on" > "$conservation_mode_location"
-        echo "Conservation mode turned ON
-
-================ CONSERVATION MODE ================"
+        echo "Conservation mode turned ON"
     else
-        echo "Bye
-
-================ CONSERVATION MODE ================"
+        echo "Bye"
     fi
 fi
